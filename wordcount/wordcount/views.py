6b63b34e74d55dbf30dd699483955aacfde05b8c
safa <hrzqrz@gmail.com>
2022-11-24 11:43:23 +0330

    settings, urls, views changed, templates folder added to project

--- wordcount/wordcount/views.py
+++ wordcount/wordcount/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def home(request):
-    return HttpResponse('Hello home')
+    return render(request, 'home.html', {'safa': 'This is me'})
 
 def eggs(request):
     return HttpResponse('THis is eggs page')
