b6a9d6503dead59a33c8bbc739e7da1ca606e357
safa <hrzqrz@gmail.com>
2022-11-24 13:58:11 +0330

    file home, urls, views, counted added to project

--- wordcount/wordcount/urls.py
+++ wordcount/wordcount/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path('eggs', views.eggs),
+    path('count/', views.count, name='count')
+
 ]
